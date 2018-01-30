1a5076237b350c35257019b0d1b2d68864dab445
phsantiago <pedroh_s_@hotmail.com>
2018-01-30 01:32:54 +0000

    tests and calcs done

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
-const calcChange = (changeVal, wallet) => {
+const calcCoin = (name, price, changeValAcc, wallet) => {
+  const coinQty = Math.floor(changeValAcc / price)
+
+  let coin = 0;
+  let newChangeVal = changeValAcc;
+
+  if(wallet && wallet[name] == 0){
+    coin = 0;
+  }
+  if(wallet && wallet[name] <= coinQty){
+    coin = wallet[name];
+  }
+  if(wallet && wallet[name] > coinQty){
+    coin = coinQty;
+  }
+
+  newChangeVal -= (price * coin);
+
+  if (changeValAcc != newChangeVal) {
+    return {
+      coin: coin,
+      changeValAcc: parseFloat(newChangeVal.toFixed(2)),
+    }
+  }
 
   return {
-    "1centavo": 0,
-    "5centavos": 1,
-    "10centavos": 0,
-    "25centavos": 0,
-    "50centavos": 1,
-    "1real": 3,
+    coin: 0,
+    changeValAcc: changeValAcc,
   }
 }
+
+const calcChange = (changeVal, wallet) => {
+  let changeValAcc = changeVal;
+  const coinAcc = {
+    real: 0,
+    centavos50: 0,
+    centavos25: 0,
+    centavos10: 0,
+    centavos5: 0,
+    centavo: 0,
+  }
+
+  let result = calcCoin('real', 1, changeValAcc, wallet);
+  coinAcc.real = result.coin;
+  changeValAcc = result.changeValAcc;
+
+  result = calcCoin('centavos50', 0.50, changeValAcc, wallet);
+  coinAcc.centavos50 = result.coin;
+  changeValAcc = result.changeValAcc;
+
+  result = calcCoin('centavos25', 0.25, changeValAcc, wallet);
+  coinAcc.centavos25 = result.coin;
+  changeValAcc = result.changeValAcc;
+
+  result = calcCoin('centavos10', 0.10, changeValAcc, wallet);
+  coinAcc.centavos10 = result.coin;
+  changeValAcc = result.changeValAcc;
+
+  result = calcCoin('centavos5', 0.05, changeValAcc, wallet);
+  coinAcc.centavos5 = result.coin;
+  changeValAcc = result.changeValAcc;
+
+  result = calcCoin('centavo', 0.01, changeValAcc, wallet);
+  coinAcc.centavo = result.coin;
+
+  return coinAcc;
+}
+const wallet = {
+  'centavo': 20,
+  'centavos5': 13,
+  'centavos10': 2,
+  'centavos25': 22,
+  'centavos50': 1,
+  'real': 14,
+}
+
+console.log(calcChange(3.55, wallet));
 
 module.exports = calcChange;
--- index.test.js
+++ index.test.js
@@ -2,26 +2,40 @@ const calcChange = require('./index.js');
 
 describe('calcChange', () => {
   it('should return a object with kinds of coins availble', () => {
-    expect(Object.keys(calcChange()).sort()).toEqual(['1centavo', '5centavos', '10centavos', '25centavos', '50centavos', '1real'].sort());
+    expect(Object.keys(calcChange()).sort()).toEqual(['centavo', 'centavos5', 'centavos10', 'centavos25', 'centavos50', 'real'].sort());
   })
 
   it('should return 3 coins of kind "1real", one of 50 and one of 5 when receive 3.55', () => {
     const wallet = {
-      '1centavo': 20,
-      '5centavos': 13,
-      '10centavos': 2,
-      '25centavos': 22,
-      '50centavos': 1,
-      '1real': 14,
+      'centavo': 20,
+      'centavos5': 13,
+      'centavos10': 2,
+      'centavos25': 22,
+      'centavos50': 1,
+      'real': 14,
+    }
+    expect(calcChange(3.55, wallet).real).toBe(3);
+    expect(calcChange(3.55, wallet).centavos50).toBe(1);
+    expect(calcChange(3.55, wallet).centavos5).toBe(1);
+  });
+
+  it('should propely calc 12 (10 real and 200 cents)', () => {
+    const wallet = {
+      'centavo': 200,
+      'centavos5': 0,
+      'centavos10': 0,
+      'centavos25': 0,
+      'centavos50': 0,
+      'real': 10,
     }
     const result = {
-      '1centavo': 0,
-      '5centavos': 1,
-      '10centavos': 0,
-      '25centavos': 0,
-      '50centavos': 1,
-      '1real': 3,
+      'centavo': 200,
+      'centavos5': 0,
+      'centavos10': 0,
+      'centavos25': 0,
+      'centavos50': 0,
+      'real': 10,
     }
-    expect(calcChange(3.55, wallet)).toEqual(result);
-  })
+    expect(calcChange(12, wallet)).toEqual(result);
+  });
 })
